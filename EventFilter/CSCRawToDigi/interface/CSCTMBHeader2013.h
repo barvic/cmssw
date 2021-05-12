9c5b668a7518f2307f4db3b861fb6576c35f150a
Victor <barvic@ufl.edu>
2021-05-12 22:03:09 +0200

    Updated CSC-GEM unpacking/DQM tests branch for CMSSW_12_X

--- EventFilter/CSCRawToDigi/interface/CSCTMBHeader2013.h
+++ EventFilter/CSCRawToDigi/interface/CSCTMBHeader2013.h
@@ -26,6 +26,19 @@ struct CSCTMBHeader2013 : public CSCVTMBHeaderFormat {
   uint16_t syncErrorMPC0() const override { return bits.MPC_Muon0_SyncErr_; }
   uint16_t syncErrorMPC1() const override { return bits.MPC_Muon1_SyncErr_; }
 
+  /// == Run 3 CSC-GEM Trigger Format
+  virtual uint16_t CLCT0_ComparatorCode() const override { return 0; }
+  virtual uint16_t CLCT1_ComparatorCode() const override { return 0; }
+  virtual uint16_t CLCT0_xky() const override { return 0; }
+  virtual uint16_t CLCT1_xky() const override { return 0; }
+  virtual uint16_t HMT_nhits() const override { return 0; }
+  virtual uint16_t GEM_enabled_fibers() const override { return 0; }
+  virtual uint16_t GEM_fifo_tbins() const override { return 0; }
+  virtual uint16_t GEM_fifo_pretrig() const override { return 0; }
+  virtual uint16_t GEM_zero_suppress() const override { return 0; }
+  virtual uint16_t GEM_sync_dataword() const override { return 0; }
+  virtual uint16_t GEM_timing_dataword() const override { return 0; }
+
   ///returns CLCT digis
   std::vector<CSCCLCTDigi> CLCTDigis(uint32_t idlayer) override;
   ///returns CorrelatedLCT digis
