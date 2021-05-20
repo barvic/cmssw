0a1ee6a7ae893c541890f07651553825e18be681
Victor <barvic@ufl.edu>
2021-05-20 16:08:28 +0200

    Testing GEMDetId GEMPadDigiCluster digis collection for b904

--- EventFilter/CSCRawToDigi/plugins/CSCDCCUnpacker.cc
+++ EventFilter/CSCRawToDigi/plugins/CSCDCCUnpacker.cc
@@ -564,9 +564,10 @@ void CSCDCCUnpacker::produce(edm::Event& e, const edm::EventSetup& c) {
                   for (int unsigned igem = 0; igem < (int unsigned)(cscData[iCSC].tmbData()->gemData()->numGEMs());
                        ++igem) {
                     /// !!! TODO: Needs mapping for CSCDetId to GEMDetId
-                    int gem_chamber = igem;
+                    int gem_chamber = layer.chamber();
+                    int gem_region = (layer.endcap()==1)?1:-1;  
                     if (b904Setup) {
-                      GEMDetId gemid(1, 1, 1, 1, gem_chamber, 1); /// Dummy id for b904
+                      GEMDetId gemid(gem_region, layer.ring(), layer.station(), igem+1, gem_chamber, 0); /// Dummy id for b904
                       std::vector<GEMPadDigiCluster> gemDigis = cscData[iCSC].tmbData()->gemData()->digis(igem);
                       gemProduct->move(std::make_pair(gemDigis.begin(), gemDigis.end()), gemid);
                     }
